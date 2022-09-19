96945f0fbb81a3322e3a49eb7dc40e1859ed511f
vuong <vuongtt@synodus.com>
2022-09-19 03:50:22 +0000

    edit

--- vault-plugin-secrets-mock/cmd/vault-plugin-secrets-mock/main.go
+++ vault-plugin-secrets-mock/cmd/vault-plugin-secrets-mock/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 
 	"github.com/hashicorp/go-hclog"
-	mock "github.com/vuongtt122/vault-plugins/vault-plugin-secrets-mock"
+	mock "github.com/hashicorp/vault-guides/plugins/vault-plugin-secrets-mock"
 	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/plugin"
 )
