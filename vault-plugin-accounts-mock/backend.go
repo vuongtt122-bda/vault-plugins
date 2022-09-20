9648848dcaa08583f71da94fae6a321ab4ec2df9
vuong <vuongtt@synodus.com>
2022-09-20 10:36:47 +0000

    update path account & sign

--- vault-plugin-accounts-mock/backend.go
+++ vault-plugin-accounts-mock/backend.go
@@ -50,17 +50,216 @@ func newBackend() (*backend, error) {
 		Help:        strings.TrimSpace(mockHelp),
 		BackendType: logical.TypeLogical,
 		Paths: framework.PathAppend(
-			b.paths(),
+			b.accountPaths(),
+			b.signPaths(),
 		),
 	}
 
 	return b, nil
 }
 
+func (b *backend) signPaths() []*framework.Path {
+	return []*framework.Path{
+		{
+			Pattern: "sign",
+
+			Fields: map[string]*framework.FieldSchema{
+				"message": {
+					Type:        framework.TypeString,
+					Description: "Specifies the path of the secret.",
+				},
+				"accountId": {
+					Type:        framework.TypeString,
+					Description: "Specifies the accountId of the secret.",
+				},
+			},
+
+			Operations: map[logical.Operation]framework.OperationHandler{
+				logical.ReadOperation: &framework.PathOperation{
+					Callback: b.handleReadSign,
+					Summary:  "Retrieve the secret from the map.",
+				},
+			},
+
+			ExistenceCheck: b.handleExistenceCheckSign,
+		},
+	}
+}
+
+func (b *backend) handleExistenceCheckSign(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(ctx, req.Path)
+	if err != nil {
+		return false, errwrap.Wrapf("existence check failed: {{err}}", err)
+	}
+
+	return out != nil, nil
+}
+
+func (b *backend) handleReadSign(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	accountId := data.Get("accountId").(string)
+	message := data.Get("message").(string)
+
+	// Decode the data
+	var rawData Account
+	entry, err := req.Storage.Get(ctx, req.ClientToken + "/" + accountId) // ignore: req.ClientToken + "/" + accountId
+	if err != nil {
+		return nil, err
+	}
+	fetchedData := entry.Value
+	if fetchedData == nil {
+		resp := logical.ErrorResponse("No value at %v%v", req.MountPoint, accountId)
+		return resp, nil
+	}
+
+	if err := jsonutil.DecodeJSON(fetchedData, &rawData); err != nil {
+		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
+	}
+
+
+	b.Logger().Info(fmt.Sprintf("\n\n %v \n\n", rawData.PrivateKey))
+
+	signatureEncode64 := message + "-" + rawData.PrivateKey + "-" + rawData.PublicKey
+	resData := map[string]interface{}{
+		"signatureEncode64": signatureEncode64,
+	}
+	// Generate the response
+	resp := &logical.Response{
+		Data: resData,
+	}
+
+	return resp, nil
+}
+
+func (b *backend) accountPaths() []*framework.Path {
+	return []*framework.Path{
+		{
+			Pattern: "account",
+
+			Fields: map[string]*framework.FieldSchema{
+				// "path": {
+				// 	Type:        framework.TypeString,
+				// 	Description: "Specifies the path of the secret.",
+				// },
+				"accountId": {
+					Type:        framework.TypeString,
+					Description: "Specifies the accountId of the secret.",
+				},
+			},
+
+			Operations: map[logical.Operation]framework.OperationHandler{
+				logical.ReadOperation: &framework.PathOperation{
+					Callback: b.handleReadAccount,
+					Summary:  "Retrieve the secret from the map.",
+				},
+				logical.CreateOperation: &framework.PathOperation{
+					Callback: b.handleWriteAccount,
+				},
+			},
+
+			ExistenceCheck: b.handleExistenceCheckAccount,
+		},
+	}
+}
+
+func (b *backend) handleExistenceCheckAccount(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(ctx, req.Path)
+	if err != nil {
+		return false, errwrap.Wrapf("existence check failed: {{err}}", err)
+	}
+
+	return out != nil, nil
+}
+
+func (b *backend) handleReadAccount(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	accountId := data.Get("accountId").(string)
+
+	// Decode the data
+	var rawData Account
+	entry, err := req.Storage.Get(ctx, req.ClientToken + "/" + accountId) // ignore: req.ClientToken + "/" + accountId
+	if err != nil {
+		return nil, err
+	}
+	fetchedData := entry.Value
+	if fetchedData == nil {
+		resp := logical.ErrorResponse("No value at %v%v", req.MountPoint, accountId)
+		return resp, nil
+	}
+
+	if err := jsonutil.DecodeJSON(fetchedData, &rawData); err != nil {
+		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
+	}
+
+
+	b.Logger().Info(fmt.Sprintf("\n\n %v \n\n", rawData.PrivateKey))
+
+	signatureEncode64 := rawData.PrivateKey + "-" + rawData.PublicKey
+	resData := map[string]interface{}{
+		"signatureEncode64": signatureEncode64,
+	}
+	// Generate the response
+	resp := &logical.Response{
+		Data: resData,
+	}
+
+	return resp, nil
+}
+
+func (b *backend) handleWriteAccount(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	// Check to make sure that kv pairs provided
+	if len(req.Data) == 0 {
+		return nil, fmt.Errorf("data must be provided to store in secret")
+	}
+
+	accountId := data.Get("accountId").(string)
+
+	// JSON encode the data
+	newAccount := Account{AccountId: accountId, PublicKey: "0x1234", PrivateKey: "0x9876"}
+	b.Logger().Info(fmt.Sprintf("\n\n ClientToken=%v \n accountId=%v \n\n", req.ClientToken, accountId))
+
+	buf, err := json.Marshal(newAccount)
+	if err != nil {
+		return nil, errwrap.Wrapf("json encoding failed: {{err}}", err)
+	}
+
+	// Store kv pairs in map at specified path
+	entry := &logical.StorageEntry{
+		Key:      req.ClientToken + "/" + accountId,
+		Value:    buf,
+		SealWrap: false,
+	}
+	if err = req.Storage.Put(ctx, entry); err != nil {
+		return nil, err
+	}
+
+	resData := map[string]interface{}{
+		"accountId": newAccount.AccountId,
+		"status": true,
+		"txHash": newAccount.PrivateKey,
+	}
+	// Generate the response
+	resp := &logical.Response{
+		Data: resData,
+	}
+
+	return resp, nil
+}
+
 func (b *backend) paths() []*framework.Path {
 	return []*framework.Path{
 		{
-			Pattern: "account" + framework.MatchAllRegex("path"),
+			Pattern: framework.MatchAllRegex("path"),
 
 			Fields: map[string]*framework.FieldSchema{
 				"path": {
